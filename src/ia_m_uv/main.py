3769b8f17f9b88066e68c912a9bf58d57ca48985
chiarorosa <pablo.chiaro.rosa@gmail.com>
2025-05-30 11:28:55 -0300

    fix:uv

---
+++ src/ia_m_uv/main.py
@@ -0,0 +1,29 @@
+import sys
+
+from .utils import parse_args
+
+"""
+  Exemplo de utilização:
+  uv run ia-m-uv -p 'prompt aqui'
+  
+  O arquivo main.py será chamado, nele a utilização do 'utils' é vista, além de ser possível a partir dele chamar outros algoritmos, até mesmo passar algum argumento texto:
+  
+  -p OU --problema 'prompt vai aqui'
+"""
+
+
+def main() -> None:
+    print("Meu projeto!")
+    args = parse_args()
+
+    try:
+        problema = args.problema
+        print(f"Problema: {problema}")
+        return 0
+    except Exception as e:
+        print(f"Erro: {e}")
+        return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
